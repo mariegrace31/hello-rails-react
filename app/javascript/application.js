bbf0f6089e9c9fbe3020861a4aa4209209144cf8
mariegrace <mariegracebmg@gmail.com>
2023-11-22 23:09:06 +0200

    Updat dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
@@ -9,10 +9,10 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-  <Provider store={store}>
-     <App/>
-  </Provider>
-</BrowserRouter>,
-document.getElementById('root'),
+ <BrowserRouter>
+   <Provider store={store}>
+      <App/>
+   </Provider>
+ </BrowserRouter>,
+  document.getElementById('root'),
 );
